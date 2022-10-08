344d4b8f245248223ff853db1147e20aa49bc9cc
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-10-08 14:27:03 +0530

    Choice Select Login done

--- index.js
+++ index.js
@@ -3,23 +3,72 @@
 let ham = document.querySelector('#ham').onclick = hamOpen
 let cross = document.getElementById('ham-cross').onclick = hamOpen
 
+let textChoice = document.querySelector('#text-choice')
+let imageChoice = document.querySelector('#image-choice')
+let videosChoice = document.querySelector('#videos-choice')
 
-
+textChoice.onclick = textSelect
+imageChoice.onclick = imageSelect
+videosChoice.onclick = videosSelect
 
 let open = false
+let text = false
+let image = false
+let videos = false
 
 
-function hamOpen(){
+function hamOpen() {
 
     open = !open
 
     let sidebar = document.querySelector('.sidebar')
 
-    if( open ){
+    if (open) {
         sidebar.style.transform = "translateX(0%)"
     }
-    else{
+    else {
         sidebar.style.transform = "translateX(100%)"
     }
 }
 
+function textSelect() {
+
+    if (textChoice.classList.contains("sub-nav-items-active")) {
+
+        textChoice.classList.remove("sub-nav-items-active")
+        console.log(textChoice.classList);
+        return
+    }
+
+    textChoice.classList += " sub-nav-items-active"
+    console.log(textChoice.classList);
+
+}
+
+function imageSelect() {
+
+    if (imageChoice.classList.contains("sub-nav-items-active")) {
+
+        imageChoice.classList.remove("sub-nav-items-active")
+        console.log(imageChoice.classList);
+        return
+    }
+
+    imageChoice.classList += " sub-nav-items-active"
+    console.log(imageChoice.classList);
+
+}
+
+function videosSelect() {
+
+    if (videosChoice.classList.contains("sub-nav-items-active")) {
+
+        videosChoice.classList.remove("sub-nav-items-active")
+        console.log(videosChoice.classList);
+        return
+    }
+
+    videosChoice.classList += " sub-nav-items-active"
+    console.log(videosChoice.classList);
+
+}
