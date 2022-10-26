6298d53a795c71b8e288c8b3e8032d0167316487
Ritesh Yadav <riteshyadav070802@gmail.com>
2022-10-26 09:50:11 +0530

    signup.html login.js updated

--- JavaScript_Files/login.js
+++ JavaScript_Files/login.js
@@ -1,12 +1,7 @@
-
-
-
-    // console.log(app);
-    
-    
+  
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+    import { getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,33 +21,6 @@
     // console.log(app);r
     const auth = getAuth();
 
-    // //----- New Registration code start	  
-    // document.getElementById("signup").addEventListener("click", function () {
-    //     var email = document.getElementById("email").value;
-    //     var password = document.getElementById("pwd").value;
-    //     //For new registration
-    //     createUserWithEmailAndPassword(auth, email, password)
-    //         .then((userCredential) => {
-    //             // Signed in 
-    //             const user = userCredential.user;
-    //             console.log(user);
-    //             alert("Registration successfully!!");
-    //             // ...
-    //         })
-    //         .catch((error) => {
-    //             const errorCode = error.code;
-    //             const errorMessage = error.message;
-    //             // ..
-    //             console.log(errorMessage);
-    //             alert(error);
-    //         });
-    // });
-    // //----- End
-
-    //----- Login code start
-
-
-
     document.getElementById("login").addEventListener("click", function () {
         var email = document.getElementById("email1").value;
         var password = document.getElementById("pwd1").value;
@@ -61,11 +29,12 @@
             .then((userCredential) => {
                 // Signed in 
                 const user = userCredential.user;
+                
                 localStorage.setItem('user', JSON.stringify(user))
                 console.log(user);
                 alert(user.email + " Login successfully!!!");
                 // login();
-                window.location.href = "../index.html";
+                window.location.href = "./user_profile.html";
                 showSession()
                 // ...
             })
@@ -84,20 +53,7 @@
 
     
 
-    //----- End
 
-    // //----- Logout code start	  
-    // document.getElementById("logout").addEventListener("click", function () {
-    //     signOut(auth).then(() => {
-    //         // Sign-out successful.
-    //         console.log('Sign-out successful.');
-    //         alert('Sign-out successful.');
-    //         document.getElementById('logout').style.display = 'none';
-    //     }).catch((error) => {
-    //         // An error happened.
-    //         console.log('An error happened.');
-    //     });
-    // });
 
 
       
