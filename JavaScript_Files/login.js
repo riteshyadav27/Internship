582c391afde0c1e6635a67844e28f718267efc97
Ritesh Yadav <riteshyadav070802@gmail.com>
2022-11-05 17:50:19 +0530

    data fetched and display...

--- JavaScript_Files/login.js
+++ JavaScript_Files/login.js
@@ -1,88 +1,90 @@
-  
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-    import { getAuth,GoogleAuthProvider,signInWithPopup, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyCk1WBSSC7TDHt-PscckKwPLsO-KpsWeuA",
-        authDomain: "login-auth-a4895.firebaseapp.com",
-        databaseURL: "https://login-auth-a4895-default-rtdb.firebaseio.com",
-        projectId: "login-auth-a4895",
-        storageBucket: "login-auth-a4895.appspot.com",
-        messagingSenderId: "488435744810",
-        appId: "1:488435744810:web:97f4cd598a461001e4c353"
-      };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set, push, child, onValue } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCk1WBSSC7TDHt-PscckKwPLsO-KpsWeuA",
+  authDomain: "login-auth-a4895.firebaseapp.com",
+  databaseURL: "https://login-auth-a4895-default-rtdb.firebaseio.com",
+  projectId: "login-auth-a4895",
+  storageBucket: "login-auth-a4895.appspot.com",
+  messagingSenderId: "488435744810",
+  appId: "1:488435744810:web:97f4cd598a461001e4c353"
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-    // console.log(app);r
-    const auth = getAuth();
-
-    document.getElementById("login").addEventListener("click", function () {
-        var email = document.getElementById("email1").value;
-        var password = document.getElementById("pwd1").value;
-
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                
-                localStorage.setItem('user', JSON.stringify(user))
-                console.log(user);
-                alert(user.email + " Login successfully!!!");
-                // login();
-                window.location.href = "/index.html";
-                showSession()
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorMessage);
-                alert(errorMessage);
-            });
-    });
-
-    function showSession(){
-        console.log(localStorage.getItem("user"));
-    }
-
-
-    
-    const provider = new GoogleAuthProvider(app);
-
-loginbtn.addEventListener('click',(e)=>{
-
+// console.log(app);
 const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    alert(user + 'Login Successfully')
-    window.location.href = "/index.html";
+const db = getDatabase();
 
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+document.getElementById("login").addEventListener("click", function () {
+  var email = document.getElementById("email1").value;
+  var password = document.getElementById("pwd1").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+
+      localStorage.setItem('user', JSON.stringify(user))
+      console.log(user);
+      localStorage.setItem('UID',user.uid)
+      alert(user.email + " Login successfully!!!");
+      // login();
+      window.location.href = "../HTML_Files/user_profile.html";
+      showSession()
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    });
+});
+
+function showSession() {
+  console.log(localStorage.getItem("user"));
+
+}
+
+
+
+const provider = new GoogleAuthProvider(app);
+
+loginbtn.addEventListener('click', (e) => {
+
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      alert(user + 'Login Successfully')
+      window.location.href = "/index.html";
+
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 
 })
 
 
 
 
-      
